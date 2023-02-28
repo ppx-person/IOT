f9ee9feea13fa475c50e8adb701b0daa811145f6
deng <6882855@qq.com>
2023-02-28 17:46:31 +0800

    Signed-off-by: deng <6882855@qq.com>

---
+++ money/python/draw.py
@@ -0,0 +1,31 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+
+
+
+
+
+
+
+
+
+
+
+png = mpimg.imread("D:\\tmp\\0.png")
+
+# print(dir(png))
+
+print(png.shape)
+show = plt.imshow(png)
+print(dir(show))
+
+
+
+# plt.show()
+
+
+
+
